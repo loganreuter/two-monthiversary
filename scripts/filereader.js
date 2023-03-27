0e2438098ccf2ac651c2235792b932375d41044c
lreuter2020 <lreuter2020@gmail.com>
2023-03-27 04:19:35 -0700

    switched to fetch request

--- scripts/filereader.js
+++ scripts/filereader.js
@@ -4,25 +4,35 @@ const FOLDER = "https://api.github.com/repos/lreuter2020/two-monthiversary/conte
 let images = []
 
 export const get_images = new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", FOLDER, true);
-    xhr.responseType = "document";
-    xhr.onload = () => {
-        if(xhr.status == 200){
-            xhr.response.json().then((res) => {
-                for(let img of xhr.response){
-                    // console.log(img, img.title, img.title.match(/\.(jpe?g|png)$/i))
-                    if(img.name.match(/\.(jpe?g|png)$/i)){
-                        // images.push(img.href)
-                        images.push(img.html_url)
-                    }
+    fetch(FOLDER)
+        .then((res) => res.json())
+        .then((data) => {
+            for(let img of data){
+                if(img.title.match(/\.(jpe?g|png)$/i)){
+                    images.push(img.html_url)
                 }
-                resolve(images)
-            })
-        } else {
-            reject("Failed to get images")
-        }
-    }
+            }
+            resolve(images)
+        }).catch((err) => {
+            reject(err.message)
+        })
+    // let xhr = new XMLHttpRequest();
+    // xhr.open("GET", FOLDER, true);
+    // xhr.responseType = "document";
+    // xhr.onload = () => {
+    //     if(xhr.status == 200){
+    //         for(let img of xhr.response){
+    //             // console.log(img, img.title, img.title.match(/\.(jpe?g|png)$/i))
+    //             if(img.title.match(/\.(jpe?g|png)$/i)){
+    //                 // images.push(img.href)
+    //                 images.push(img.href)
+    //             }
+    //         }
+    //         resolve(images)
+    //     } else {
+    //         reject("Failed to get images")
+    //     }
+    // }
 
-    xhr.send()
+    // xhr.send()
 })
